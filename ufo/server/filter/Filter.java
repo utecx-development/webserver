9d64a1338ed1d0da34c5da32e8d3e2508593ba1d
UfoMc <99866680+UfoMc@users.noreply.github.com>
2024-11-07 17:10:53 +0100

    Delete ufo directory

--- ufo/server/filter/Filter.java
+++
@@ -1,7 +0,0 @@
-package dev.ufo.server.filter;
-
-import dev.ufo.server.object.Request;
-
-public interface Filter {
-    boolean doFilter(Request req);
-}
--- ufo/test/HtmlTest.java
+++
@@ -1,9 +0,0 @@
-package dev.ufo.test;
-
-import dev.ufo.usr.annotation.Route;
-import dev.ufo.usr.annotation.RouteType;
-import dev.ufo.usr.pathimp.Path;
-
-@Route(path = "/", type = RouteType.HTML)
-public class HtmlTest implements Path {
-}
